3b5e0f81472fc1865ac9c332993061a34dac7244
nguyenquocdat13062007-netizen <nguyenquocdat13062007@gmail.com>
2026-07-31 10:26:18 +0700

    Refactor SelectionS class implementation

--- SelectionS.h
+++ SelectionS.h
@@ -8,14 +8,11 @@ protected:
         int n = this->array.size();
 
         for (int i = 0; i < n - 1; i++) {
-            // Tim vi tri phan tu nho nhat trong array[i+1 .. n-1]
             int minIdx = i;
             for (int j = i + 1; j < n; j++) {
                 if (this->array[j] < this->array[minIdx])
                     minIdx = j;
             }
-
-            // Hoan doi array[i] va array[minIdx] neu can
             if (minIdx != i)
                 std::swap(this->array[i], this->array[minIdx]);
         }
