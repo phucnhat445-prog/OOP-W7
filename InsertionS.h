55fde5537778b9fb91a9ca4fad205ceb9f96b658
nguyenquocdat13062007-netizen <nguyenquocdat13062007@gmail.com>
2026-07-31 10:25:45 +0700

    Refactor InsertionS class implementation

--- InsertionS.h
+++ InsertionS.h
@@ -8,17 +8,12 @@ protected:
         int n = this->array.size();
 
         for (int i = 1; i < n; i++) {
-            // Lay phan tu hien tai can chen vao phan da sap xep
             T key = this->array[i];
             int j = i - 1;
-
-            // Dich phan tu lon hon key sang phai 1 vi tri
             while (j >= 0 && this->array[j] > key) {
                 this->array[j + 1] = this->array[j];
                 j--;
             }
-
-            // Dat key vao vi tri dung
             this->array[j + 1] = key;
         }
     }
